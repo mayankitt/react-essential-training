bc896a51aa408ab5e2a7d73d6b2e0268006d36fa
Mayank Choudhary <mayankitt@users.noreply.github.com>
2024-12-30 08:18:55 +0000

    Completed - Adding keys to list items

--- chapters/02_01b/react-project/src/App.jsx
+++ chapters/02_01b/react-project/src/App.jsx
@@ -9,12 +9,35 @@ function Header({name, year}) {
   );
 }
 
+const items = [
+  "Dal Bhaat chokha",
+  "Dal Pithi",
+  "Dal Makhni",
+  "Aalu paratha"
+];
+
+const dishObjects = items.map((item, index) => ({
+  id: index,
+  title: item
+}))
+
+function Main({dishes}) {
+  return (
+    <ul>
+      {dishes.map((dish) => (
+        <li style={{ listStyleType: "none" }} key={dish.id}>{dish.title}</li>
+      ))}
+    </ul>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header name="Mayank" year={new Date().getFullYear()}/>
       <main>
         <h2>We serve the most delicious food around!</h2>
+        <Main dishes={dishObjects} />
       </main>
     </div>
   );
